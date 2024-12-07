3b7f20a42d57206c4931b1b6f9f63c7757e4178a
fergal <fergal.okane@yahoo.com>
2024-12-07 23:54:19 +0100

    Day 7 part 2

--- day_7/main.cpp
+++ day_7/main.cpp
@@ -22,12 +22,16 @@ int main(int argc,  char** argv) {
             vals.push_back(val);
         }
 
-        long final = 1 << vals.size();
+        // Very slow. Up to 4**n combinations instead of 3**n and no optimisations...
+        long final = 1 << (2 * vals.size());
         for (long combination = 0; combination < final; combination++) {
             long test_val = vals[0];
             for (size_t i = 1; i < vals.size(); i++) {
-                if (combination & (1 << i)) test_val *= vals[i];
-                else test_val += vals[i];
+                if (combination & (1 << (2*i+1))) test_val *= vals[i];
+                else if (combination & (1 << (2*i))) test_val += vals[i];
+                else test_val = std::stol(std::to_string(test_val) + std::to_string(vals[i]));
+
+                if (test_val > target) break;
             }
             if (test_val == target) {
                 result += target;
