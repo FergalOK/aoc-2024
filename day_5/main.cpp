63bc0aee094062eff50ca42a4740924840e90f43
fergal <fergal.okane@yahoo.com>
2024-12-07 19:17:47 +0100

    Day 5 part 2

--- day_5/main.cpp
+++ day_5/main.cpp
@@ -26,7 +26,7 @@ int main(int argc,  char** argv) {
         precedence[after].push_back(before);
     }
 
-    auto calculate = [precedence](std::vector<int> pages) {
+    auto calculate_part1 = [precedence](std::vector<int> pages) {
         std::unordered_set<int> blacklist;
         for (int page : pages) {
             if (blacklist.find(page) != blacklist.end()) {
@@ -42,8 +42,35 @@ int main(int argc,  char** argv) {
         return pages[pages.size() / 2];
     };
 
+    auto calculate_part2 = [precedence](std::vector<int> pages) {
+        bool sorted = false;
+        while (!sorted) {
+            std::unordered_map<int, int> blacklist;
+            sorted = true;
+            for (size_t i = 0; i < pages.size(); i++) {
+                int page = pages[i];
+                if (blacklist.find(page) != blacklist.end()) {
+                    int j = blacklist[page];
+                    int tmp = pages[i];
+                    pages[i] = pages[j];
+                    pages[j] = tmp;
+                    sorted = false;
+                    break;
+                }
 
-    int result = 0;
+                if (precedence.find(page) != precedence.end()) {
+                    for (int p : precedence.at(page)) {
+                        blacklist.insert({p, i});
+                    }
+                }
+            }
+        }
+
+        return pages[pages.size() / 2];
+    };
+
+    int result_ordered = 0;
+    int result_unordered = 0;
     while (std::getline(file, line)) {
         std::vector<int> pages;
         auto stream = std::stringstream(line);
@@ -53,8 +80,11 @@ int main(int argc,  char** argv) {
             pages.push_back(page);
         }
 
-        result += calculate(pages);
+        int val = calculate_part1(pages);
+        result_ordered += val;
+        if (!val) result_unordered += calculate_part2(pages);
     }
 
-    std::cout << "Result " << result << std::endl;
+    std::cout << "Result " << result_ordered << std::endl;
+    std::cout << "Result " << result_unordered << std::endl;
 }
