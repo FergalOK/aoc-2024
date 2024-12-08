59b4c4ef945a99d217aa982fa6eb984378d1be3e
fergal <fergal.okane@yahoo.com>
2024-12-08 14:41:48 +0100

    Day 8 part 2

--- day_8/main.cpp
+++ day_8/main.cpp
@@ -30,14 +30,20 @@ int main(int argc,  char** argv) {
     for (auto freq : a) {
         std::vector<std::pair<int,int>> positions = freq.second;
         for (size_t i = 0; i < positions.size(); i++) {
-            for (size_t j = i+1; j < positions.size(); j++) {
-                int x0 = 2 * positions[i].first - positions[j].first;
-                int y0 = 2 * positions[i].second - positions[j].second;
-                if (x0 >= 0 && x0 < width && y0 >= 0 && y0 < height) antinodes.insert(hash(x0, y0));
-
-                int x1 = 2 * positions[j].first - positions[i].first;
-                int y1 = 2 * positions[j].second - positions[i].second;
-                if (x1 >= 0 && x1 < width && y1 >= 0 && y1 < height) antinodes.insert(hash(x1, y1));
+            for (size_t j = 0; j < positions.size(); j++) {
+                if (i == j) {
+                    antinodes.insert(hash(positions[i].first, positions[i].second));
+                    continue;
+                }
+                int dx = positions[i].first - positions[j].first;
+                int dy = positions[i].second - positions[j].second;
+                int x = positions[i].first + dx;
+                int y = positions[i].second + dy;
+                while (x >= 0 && x < width && y >= 0 && y < height) {
+                    antinodes.insert(hash(x, y));
+                    x += dx;
+                    y += dy;
+                }
             }
         }
     }
