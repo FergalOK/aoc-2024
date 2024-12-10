c886ddf7fe3564fb345ae627dba0ba41b2e00286
fergal <fergal.okane@yahoo.com>
2024-12-10 01:51:48 +0100

    Day 9 part 2

--- day_9/main.cpp
+++ day_9/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
 #include <cassert>
-#include <unordered_map>
 #include <unordered_set>
+
+long part_1(std::vector<char> input);
+long part_2(std::vector<char> input);
 
 int main(int argc,  char** argv) {
     assert(argc == 2);
@@ -15,7 +16,11 @@ int main(int argc,  char** argv) {
     char val;
     while (file >> val) input.push_back(val - '0');
 
-    std::stringstream buffer;
+    std::cout << "Result (part 1): " << part_1(input) << std::endl;
+    std::cout << "Result (part 2): " << part_2(input) << std::endl;
+}
+
+long part_1(std::vector<char> input) {
     int file_id = 0;
     long compressed = 0;
     int checksum_count = 0;
@@ -23,10 +28,8 @@ int main(int argc,  char** argv) {
     int end_pointer_used = 0;
     for (int i = 0; i < end_pointer; i++) {
         if (i % 2 == 0) {
-            // TODO: No loop
-            for (int j = 0; j < input[i]; j++) {
-                compressed += checksum_count++ * file_id;
-            }
+            compressed += input[i] * long(checksum_count * file_id + (checksum_count+input[i]-1) * file_id) / 2;
+            checksum_count += input[i];
             file_id++;
         }
         else {
@@ -40,7 +43,6 @@ int main(int argc,  char** argv) {
                 if (end_pointer_used >= input[end_pointer]) {
                     end_pointer -= 2;
                     end_pointer_used = 0;
-                    // std::cout << "End pointer "<< end_pointer << ", i: " << i << std::endl;
                     if (end_pointer < i) {
                         break;
                     }
@@ -53,6 +55,41 @@ int main(int argc,  char** argv) {
         compressed += checksum_count++ * int(end_pointer / 2);
     }
 
-    std::cout << "Result " << compressed << std::endl;
+    return compressed;
+}
 
+long part_2(std::vector<char> input) {
+    int file_id = 0;
+    long compressed = 0;
+    int checksum_count = 0;
+    int end_pointer = input.size() - 1 - (input.size() - 1) % 2;
+    std::unordered_set<int> moved;
+    for (int i = 0; i < input.size(); i++) {
+        if (i % 2 == 0) {
+            if (moved.find(i) != moved.end()) {
+                file_id++;
+                checksum_count += input[i];
+                continue;
+            }
+            compressed += input[i] * long(checksum_count * file_id + (checksum_count+input[i]-1) * file_id) / 2;
+            checksum_count += input[i];
+            file_id++;
+        }
+        else {
+            int end_pointer_used = 0;
+            for (int j = end_pointer; j > i; j -= 2) {
+                if (input[j] <= input[i] - end_pointer_used && moved.find(j) == moved.end()) {
+                    moved.insert(j);
+                    for (int k = 0; k < input[j]; k++) {
+                        compressed += checksum_count++ * int(j / 2);
+                    }
+                    end_pointer_used += input[j];
+                    if (input[i] - end_pointer_used == 0) break;
+                }
+            }
+            checksum_count += input[i] - end_pointer_used;
+        }
+    }
+
+    return compressed;
 }
