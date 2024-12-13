9d0480feae2565aa68164ca7f26bcaa9f060d0dc
fergal <fergal.okane@yahoo.com>
2024-12-13 22:17:20 +0100

    Day 13 part 2

--- day_13/main.cpp
+++ day_13/main.cpp
@@ -4,19 +4,29 @@
 #include <regex>
 #include <limits.h>
 
-int calculate(int x_a, int y_a, int x_b, int y_b, int x_p, int y_p) {
-    // Note: BRUTE FORCE!
-    int min = INT_MAX;
-    for (int a = 0; a <= 100; a++) {
-        int b = (x_p - a * x_a) / x_b;
-        if (a * x_a + b * x_b == x_p && a * y_a + b * y_b == y_p) {
-            int cost = 3 * a + b;
-            if (cost < min) min = cost;
-        }
-    }
+/*
+xp = a * xa + b * xb
+yp = a * ya + b * yb
+cost = 3a + b
 
-    if (min == INT_MAX) return 0;
-    return min;
+b = (xp - a * xa) / xb
+Replacing b in second formula:
+a = (yp * xb - xp * yb) / (ya * xb - xa * yb)
+*/
+
+long calculate(long xa, long ya, long xb, long yb, long xp, long yp) {
+    long a_num = yp * xb - xp * yb;
+    long a_denom = ya * xb - xa * yb;
+    if (a_num % a_denom != 0) return 0;
+    long a = a_num / a_denom;
+    if (a < 0) return 0;
+
+    long b_num = xp - a * xa;
+    if (b_num % xb != 0) return 0;
+    long b = b_num / xb;
+    if (b < 0) return 0;
+
+    return 3 * a + b;
 }
 
 int main(int argc,  char** argv) {
@@ -26,15 +36,22 @@ int main(int argc,  char** argv) {
     buffer << file.rdbuf();
     std::string input = buffer.str();
 
-    int result = 0;
+    long result_part1 = 0;
+    long result_part2 = 0;
     std::regex reg("Button A: X\\+(\\d+), Y\\+(\\d+)\nButton B: X\\+(\\d+), Y\\+(\\d+)\nPrize: X=(\\d+), Y=(\\d+)");
     auto iter = std::sregex_iterator(input.begin(), input.end(), reg);
     for (std::sregex_iterator i = iter; i != std::sregex_iterator(); i++) {
         std::smatch match = *i;
-        result += calculate(stoi(match[1]), stoi(match[2]),
-                            stoi(match[3]), stoi(match[4]),
-                            stoi(match[5]), stoi(match[6]));
+        result_part1 += calculate(stoi(match[1]), stoi(match[2]),
+                                  stoi(match[3]), stoi(match[4]),
+                                  stol(match[5]), stol(match[6]));
+
+        result_part2 += calculate(stoi(match[1]), stoi(match[2]),
+                                  stoi(match[3]), stoi(match[4]),
+                                  10000000000000 + stol(match[5]),
+                                  10000000000000 + stol(match[6]));
     }
 
-    std::cout << "Result: " << result << std::endl;
+    std::cout << "Result (part 1): " << result_part1 << std::endl;
+    std::cout << "Result (part 2): " << result_part2 << std::endl;
 }
